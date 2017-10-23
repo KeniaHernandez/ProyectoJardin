e97c850ccbe897ad760604c3bb3fa629a28fcdc7
OscarQuintero <oscar24quintero@hotmail.com>
2017-10-23 01:33:10 -0500

    actualización de carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,5 @@
-function testfunction(){
-	this.style.backgroundColor = "red";
-}
 
-var element document.getElementById("1");
-element.onmouseover = testfunction;
+window.onload = function()
+{
+	
+}
