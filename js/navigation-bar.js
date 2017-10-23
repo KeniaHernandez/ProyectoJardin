d37ce1fff0ef660ae3b214c9d19d7db7de3cac85
OscarQuintero <oscar24quintero@hotmail.com>
2017-10-23 01:10:48 -0500

    Creación de archivo navigation-bar.js

---
+++ js/navigation-bar.js
@@ -0,0 +1,15 @@
+			function mostrarInicio(){
+				document.getElementById('inicio').style.display = 'block';
+				document.getElementById('servicios').style.display = 'none';
+				document.getElementById('productos').style.display = 'none';
+			}
+				function mostrarServicios(){
+				document.getElementById('servicios').style.display = 'block';
+				document.getElementById('inicio').style.display = 'none';
+				document.getElementById('productos').style.display = 'none';
+			}
+			function mostrarProductos(){
+				document.getElementById('productos').style.display = 'block';
+				document.getElementById('inicio').style.display = 'none';
+				document.getElementById('servicios').style.display = 'none';
+			}
